6737211469059e6594f3b4cd3815eeb36ca99861
Hammy Havoc <hammy@previousmagazine.com>
2016-07-22 07:01:11 +0100

    Initial commit.

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'mind-music-android',
+		wp_ws_url : 'https://www.mindmusic.online/wp-appkit-api/mind-music-android',
+		wp_url : 'https://www.mindmusic.online',
+		theme : 'q-android',
+		version : '1.0',
+		app_title : 'Mind Music',
+		app_platform : 'android',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'KmuHU4!Xe7M4Ex(SG8&dmSF6YM6F#jXrbqO!#*RUor681@)F2o*p9WPeqHLPurBy',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
