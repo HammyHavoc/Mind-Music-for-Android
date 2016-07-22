f43c575e9ad1ae5ca9aa5ff1ae2789a497197165
Hammy Havoc <hammy@previousmagazine.com>
2016-07-22 07:31:06 +0100

    Updated icons and splashscreens.

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ define( function ( require ) {
 		wp_ws_url : 'https://www.mindmusic.online/wp-appkit-api/mind-music-android',
 		wp_url : 'https://www.mindmusic.online',
 		theme : 'q-android',
-		version : '1.0',
+		version : '1.1',
 		app_title : 'Mind Music',
 		app_platform : 'android',
 		gmt_offset : 0,
